00830574ec02266c76901d6a9b735582aeee1445
Achiel <achiel.van.der.mandele@mendix.com>
2015-01-27 17:45:58 +0100

    tests should run without dependency on CF

--- tests/run.sh
+++ tests/run.sh
@@ -7,27 +7,26 @@ cf_app="buildpack-testing-${RANDOM}"
 cf_buildpack="https://github.com/mendix/cf-mendix-buildpack.git"
 cf_db="$cf_app-db"
 mx_admin_password="c+onT5bWvGBM34A5eUP5Dobk6uk"
-cf_mda="app.mda"
+cf_mda="tests/app/app.mda"
 cf_app_domain="cfapps.io"
 cf_app_url="https://${cf_app}.${cf_app_domain}/xas/"
 cf_memory="512M"
-cf_push_cmd="cf push ${cf_app} -m ${cf_memory} -b ${cf_buildpack} -p ${cf_mda}"
+cf_push_cmd="push ${cf_app} -m ${cf_memory} -b ${cf_buildpack} -p ${cf_mda}"
 
 set -x
 
-which cf || (echo "Cloudfoundry CLI not installed, please install it first"; exit 1)
-cf target -o $cf_org || (echo "Unable to select correct org, expecting: ${cf_org}"; exit 1)
-cf target -s $cf_space || (echo "Unable to select correct space, expecting: ${cf_space}"; exit 1)
+wget -O - "https://cli.run.pivotal.io/stable?release=linux64-binary&version=6.9.0&source=github-rel" | tar zxf -
+./cf target -o $cf_org || (echo "Unable to select correct org, expecting: ${cf_org}"; exit 1)
+./cf target -s $cf_space || (echo "Unable to select correct space, expecting: ${cf_space}"; exit 1)
 
-cd ./tests/app
-cf app ${cf_app} &>/dev/null && cf delete -f ${cf_app}
-cf service ${cf_db} &>/dev/null && cf delete-service -f ${cf_db}
+./cf app ${cf_app} &>/dev/null && cf delete -f ${cf_app}
+./cf service ${cf_db} &>/dev/null && cf delete-service -f ${cf_db}
 
-${cf_push_cmd} --no-start
-cf create-service elephantsql turtle ${cf_db}
-cf bind-service ${cf_app} ${cf_db}
-cf set-env ${cf_app} ADMIN_PASSWORD $(pwgen -y 20 1)
-${cf_push_cmd}
+./cf push ${cf_app} -m ${cf_memory} -b ${cf_buildpack} -p ${cf_mda} --no-start
+./cf create-service elephantsql turtle ${cf_db}
+./cf bind-service ${cf_app} ${cf_db}
+./cf set-env ${cf_app} ADMIN_PASSWORD $(pwgen -y 20 1)
+./cf push ${cf_app} -m ${cf_memory} -b ${cf_buildpack} -p ${cf_mda}
 
 # insert tests here
 http_code=$(curl -s -o /dev/null -w "%{http_code}" ${cf_app_url})
@@ -39,7 +38,7 @@ if [[ $http_code -ne $expected_code ]] ; then
     echo "Wrong http code $http_code, expected $expected_code"
 fi
 
-cf delete $cf_app -f
-cf delete-route $cf_app_domain -n $cf_app -f
-cf delete-service $cf_db -f
+./cf delete $cf_app -f
+./cf delete-route $cf_app_domain -n $cf_app -f
+./cf delete-service $cf_db -f
 exit $return_code
